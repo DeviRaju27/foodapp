e4dcbdc8d2b461e3bef965700e6c3ed81422281a
Devi Raju <deviraju999@gmail.com>
2024-06-08 13:23:09 +0530

    create static folder and style.css
    exit

--- menu/urls.py
+++ menu/urls.py
@@ -2,7 +2,9 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 
+app_name = 'food'
+
 urlpatterns = [
     path('/',views.index, name="index" ),
-    path('/<int:item_id>',views.detail, name="detail" ),
+    path('<int:item_id>',views.detail, name="detail" ),
 ]
